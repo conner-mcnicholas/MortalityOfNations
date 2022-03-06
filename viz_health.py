aa1deae3a291015c8836607277e1f32d025c9a1e
conner-mcnicholas <connermcnicholas@gmail.com>
2022-03-06 10:47:19 -0800

    Added python script to  view in atom hydrogen

---
+++ viz_health.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Enable the table_schema option in pandas,
+# data-explorer makes this snippet available with the `dx` prefix:
+pd.options.display.html.table_schema = True
+pd.options.display.max_rows = None
+
+#All countries
+Your dataframe here)
+fullstats = './fullstatscmp.csv'
+dff = pd.read_csv(fullstats)
+dff
+
+#Just 1st world western countries, no labels
+firstwest = './firstwestcmpr.csv'
+dfw = pd.read_csv(firstwest)
+dfw
